d42646f2a3515b288303276f7fb04a84b76e6b45
Tahmid Jihan <tahmidjihan321@gmail.com>
2024-10-23 12:26:22 +0600

    added main files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from './card';
+function App() {
+  return (
+    <>
+      <div className=''>
+        <Card></Card>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className='rounded-xl max-w-sm  bg-white shadow-xl '>
+      <div className='container p-8'>
+        <h1>Loaded content</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
